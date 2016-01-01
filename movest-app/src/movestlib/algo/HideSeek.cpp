cfae4453ede1cb529541c6dd98908fa2a0f389a6
Edgar Liberis <edgaras.liberis@gmail.com>
2016-01-01 23:16:26 +0000

    Properly handle SKIP MVs during decoding

--- movest-app/src/movestlib/algo/HideSeek.cpp
+++ movest-app/src/movestlib/algo/HideSeek.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include "HideSeek.h"
 
+#define DEC_MB_TYPE_SKIP 0x0800
+
 void HideSeek::initAsEncoder(movest_params *params) {
     Algorithm::initAsEncoder(params);
     if(!(flags & MOVEST_DUMMY_PASS)) {
@@ -47,7 +49,7 @@ void HideSeek::decode(int16_t (*mvs[2])[2], uint32_t *mbtype_table, int mv_sampl
     for (int mb_y = 0; mb_y < mb_height; mb_y++) {
         for (int mb_x = 0; mb_x < mb_width; mb_x++) {
             int xy = (mb_x + mb_y * mv_stride) << mv_sample_log2;
-            if(mbtype_table[mb_x + mb_y * mb_stride] == 2) {
+            if(!(mbtype_table[mb_x + mb_y * mb_stride] & DEC_MB_TYPE_SKIP)) {
                 extractFromMv(mvs[0][xy][0]);
                 extractFromMv(mvs[0][xy][1]);
             }
