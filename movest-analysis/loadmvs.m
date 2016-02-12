8fadbdcd65d7cdc85ecd7e94dfaad1bf13b3ad67
Edgar Liberis <edgaras.liberis@gmail.com>
2016-02-12 23:44:39 +0000

    Significantly improve performance of LoadMVs due to new output format

--- movest-analysis/loadmvs.m
+++ movest-analysis/loadmvs.m
@@ -1,57 +1,41 @@
 function [mv_x, mv_y, type] = loadmvs(input_file, varargin)
+% LOADMVS Imports motion vectors extracted by Movest Decoder
+% Specify file name as the first argument and the number of frames to
+% to process (Inf if unsure). Function can interleave x and y values
+% (although not very efficiently) if 'Interleave' vararg is set to true.
 
 p = inputParser;
 addRequired(p, 'input_file', @ischar);
 addOptional(p, 'Frames', 50, @isnumeric);
-addParameter(p, 'MBDims', [45 80], @(x) size(x) == [1 2]);
 addParameter(p, 'Interleave', false, @islogical);
 parse(p, input_file, varargin{:});
 
 maxframes = p.Results.Frames;
-mbdims = p.Results.MBDims;
 interleave = p.Results.Interleave;
 
 infile = fopen(input_file, 'r');
 
+line = fgets(infile);
+dims = textscan(line, '%d %d');
+[data, count] = fscanf(infile, '%d, %d, %d;', [3, dims{1} * dims{2} * maxframes]);
+
+framesRead = count / 3 / (dims{1} * dims{2});
+
+mv_x = reshape(data(1, :), [dims{1}, dims{2}, framesRead]);
+mv_y = reshape(data(2, :), [dims{1}, dims{2}, framesRead]);
+type = reshape(data(3, :), [dims{1}, dims{2}, framesRead]);
+
 if interleave
-    mv_x = zeros(mbdims(1), mbdims(2) * 2, maxframes); 
+    mv_xi = zeros(dims{1}, 2 * dims{2}, framesRead);
+    mv_xi(:, 1:2:end) = mv_x(:, :);
+    mv_xi(:, 2:2:end) = mv_y(:, :);
+    
+    mv_x = mv_xi;
     mv_y = [];
-    type = zeros(mbdims(1), mbdims(2) * 2, maxframes);
-else
-    mv_x = zeros(mbdims(1), mbdims(2), maxframes); 
-    mv_y = zeros(mbdims(1), mbdims(2), maxframes);
-    type = zeros(mbdims(1), mbdims(2), maxframes);
-end
-
-frame = 1;
-row = 1;
-newline = sprintf('\n');
-
-while ~feof(infile)
-    if(frame > maxframes) 
-        break;
-    end
     
-    line = fgets(infile);
+    types_i = zeros(dims{1}, 2 * dims{2}, framesRead);
+    types_i(:, 1:2:end) = type(:, :);
+    types_i(:, 2:2:end) = type(:, :);
     
-    if(strcmp(line, newline))
-        frame = frame + 1;
-        row = 1;
-    else
-        pairs = strsplit(line, '; ');
-        for k = 1:length(pairs)-1
-            xy = textscan(pairs{k}, '%d, %d, %d');
-            if interleave 
-                mv_x(row, 2*k-1, frame) = xy{1};
-                mv_x(row, 2*k, frame) = xy{2};
-                type(row, 2*k-1, frame) = xy{3};
-                type(row, 2*k, frame) = xy{3};
-            else
-                mv_x(row, k, frame) = xy{1};
-                mv_y(row, k, frame) = xy{2};
-                type(row, k, frame) = xy{3};
-            end
-        end
-        row = row + 1;
-    end
+    type = types_i;
 end
